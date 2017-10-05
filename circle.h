f3955452a7883eb605fc9795ca38d0ea711a170c
Felix Schulz <fe.schulz@gmx.net>
2017-10-05 21:46:02 +0200

    added makefile

--- circle.h
+++ circle.h
@@ -1,17 +1,11 @@
 
-#ifndef __CIRCLE_H
-#define __CIRCLE_H
-
 class Circle {
 	public:
 		Circle(int _x, int _y, int _radius);
-		~Circle();
 
 		int x, y;
 		int radius;
 
 		void render(SDL_Renderer* renderer);
 };
-
-#endif // __CIRCLE_H
 
